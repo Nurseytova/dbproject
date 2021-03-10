0d6b4a5985d0d7c35ecf8b9f99e6e81d4077196b
Untermensch5 <80197811+Untermensch5@users.noreply.github.com>
2021-03-10 16:26:23 +0600

    Update LibraryFrontEnd.java

--- src/endterm_project/LibraryFrontEnd.java
+++ src/endterm_project/LibraryFrontEnd.java
@@ -7,6 +7,15 @@ import endterm_project.repositories.interfaces.ILibraryRepository;
 import java.util.Scanner;
 
 public class LibraryFrontEnd {
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
     /*calling controller*/
     private final LibraryController controller;
     private final Scanner scanner;
@@ -21,16 +30,16 @@ public class LibraryFrontEnd {
     public void start() {
         while (true) {
             System.out.println();
-            System.out.println("Welcome to My Application");
-            System.out.println("Select option:");
-            System.out.println("1. Get all Libraries");
-            System.out.println("2. Get Library by id");
-            System.out.println("3. Create library");
-            System.out.println("4. Get all books in library by id");
-            System.out.println("0. Exit");
+            System.out.println(PURPLE + "Welcome to My Application" + RESET);
+            System.out.println(BLUE + "Select option:" + RESET);
+            System.out.println(YELLOW + "1. Get all Libraries" + RESET);
+            System.out.println(YELLOW + "2. Get book by id" + RESET);
+            System.out.println(YELLOW + "3. Create library" + RESET);
+            System.out.println(YELLOW + "4. Get all books in library by id" + RESET);
+            System.out.println(YELLOW + "0. Exit" + RESET);
             System.out.println();
             try {
-                System.out.print("Enter option (1-4): ");
+                System.out.print(BLUE + "Enter option (1-4): " + RESET);
                 int option = scanner.nextInt();
                 if (option == 1) {
                     getAllLibrariesMenu();;
@@ -56,25 +65,25 @@ public class LibraryFrontEnd {
         System.out.println(response);
     }
     public void getAllLibraryBooksMenu(){
-        System.out.println("Please enter id");
+        System.out.println(YELLOW + "Please enter id" + RESET);
         int id = scanner.nextInt();
         String response = controller.getLibraryBooks(id);
         System.out.println(response);
     }
 
     public void getLibraryByIdMenu() {
-        System.out.println("Please enter id");
+        System.out.println(YELLOW + "Please enter id" + RESET);
         int id = scanner.nextInt();
         String response = controller.getLibrary(id);
         System.out.println(response);
     }
 
     public void createLibraryMenu(){
-        System.out.println("Please enter title of book");
+        System.out.println(YELLOW + "Please enter title of book" + RESET);
         String title = scanner.next();
-        System.out.println("Please enter book's author");
+        System.out.println(YELLOW + "Please enter book's author" + RESET);
         String author = scanner.next();
-        System.out.println("Please enter year ");
+        System.out.println(YELLOW + "Please enter year " + RESET);
         int year  = scanner.nextInt();
 
         String response = controller.createLibrary(title, author,year);
